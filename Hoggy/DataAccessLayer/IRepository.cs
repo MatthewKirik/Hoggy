eceeb948a3f80731ffdc4f960e7cde3a02f99bf6
Матвей Кирик <ikirik59@gmail.com>
2019-05-20 17:38:35 +0300

    IRepository added

---
+++ Hoggy/DataAccessLayer/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        IEnumerable<T> GetList();
+        T GetItem(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save();
+    }
+}
