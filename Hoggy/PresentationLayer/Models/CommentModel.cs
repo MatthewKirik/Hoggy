9ae801e20bfa534931f0c119fa823f9168b6214f
Матвей Кирик <ikirik59@gmail.com>
2019-05-23 13:34:59 +0300

    Models was added

---
+++ Hoggy/PresentationLayer/Models/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Models
+{
+    public class CommentModel
+    {
+        public string Text { get; set; }
+        public UserModel Author { get; set; }
+        public CardModel Card { get; set; }
+    }
+}
