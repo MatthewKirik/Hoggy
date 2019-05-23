9f971337fac54b19cb59ba6998afa38c7c36f408
Матвей Кирик <ikirik59@gmail.com>
2019-05-23 13:57:08 +0300

    login window view model was initialized

--- Hoggy/PresentationLayer/ViewModels/LoginWindowViewModel.cs
+++ Hoggy/PresentationLayer/ViewModels/LoginWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,31 @@ using System.Threading.Tasks;
 
 namespace PresentationLayer.ViewModels
 {
-    public class LoginWindowViewModel
+    public class LoginWindowViewModel : ViewModelBase
     {
+        private UserModel _user;
+        public UserModel User
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                RaisePropertyChanged("User");
+            }
+        }
+
+        private RelayCommand _logInCommand;
+        public RelayCommand LogInCommand
+        {
+            get
+            {
+                _logInCommand = new RelayCommand(() =>
+                {
+
+                });
+                return _logInCommand;
+            }
+        }
 
     }
 }
