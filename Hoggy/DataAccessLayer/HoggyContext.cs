b6541522569384b1345a313bdb73dbef59a131ca
Матвей Кирик <ikirik59@gmail.com>
2019-05-23 19:06:17 +0300

    db context was added

---
+++ Hoggy/DataAccessLayer/HoggyContext.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class HoggyContext
+        : DbContext
+    {
+        public HoggyContext()
+            : base()
+        {
+
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<CardEntity> Cards { get; set; }
+        public DbSet<ColumnEntity> Columns { get; set; }
+        public DbSet<HistoryEventEntity> HistoryEvents { get; set; }
+        public DbSet<HistoryEventTypeEntity> HistoryEventTypes { get; set; }
+        public DbSet<CommentEntity> Comments { get; set; }
+        public DbSet<TagEntity> Tags { get; set; }
+    }
+}
