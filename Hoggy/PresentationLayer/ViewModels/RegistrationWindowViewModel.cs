42f63df4f0a952b38bf56c415af73d4b971f3525
Матвей Кирик <ikirik59@gmail.com>
2019-05-23 13:59:57 +0300

    registration window view model was initialized

--- Hoggy/PresentationLayer/ViewModels/RegistrationWindowViewModel.cs
+++ Hoggy/PresentationLayer/ViewModels/RegistrationWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,30 @@ using System.Threading.Tasks;
 
 namespace PresentationLayer.ViewModels
 {
-    public class RegistrationWindowViewModel
+    public class RegistrationWindowViewModel : ViewModelBase
     {
+        private UserModel _user;
+        public UserModel User
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                RaisePropertyChanged("User");
+            }
+        }
 
+        private RelayCommand _signUpCommand;
+        public RelayCommand SignUpCommand
+        {
+            get
+            {
+                _signUpCommand = new RelayCommand(() =>
+                {
+
+                });
+                return _signUpCommand;
+            }
+        }
     }
 }
