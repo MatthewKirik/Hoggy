68d1f8b7e8e09752f1f40199412741646773873d
Матвей Кирик <ikirik59@gmail.com>
2019-05-23 13:07:50 +0300

    login view and view model was added

---
+++ Hoggy/PresentationLayer/Windows/LogionWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.Windows
+{
+    /// <summary>
+    /// Interaction logic for LogionWindow.xaml
+    /// </summary>
+    public partial class LogionWindow : Window
+    {
+        public LogionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
