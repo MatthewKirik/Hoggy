9def16991e7638471c582862bf62b5e4b270e3c6
Матвей Кирик <ikirik59@gmail.com>
2019-05-24 17:15:59 +0300

    authentification contract was changed

--- Hoggy/WcfServiceLibrary/Contracts/IAuthenticationContract.cs
+++ Hoggy/WcfServiceLibrary/Contracts/IAuthenticationContract.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WcfServiceLibrary.DataTransferObjects;
 
 namespace WcfServiceLibrary.Contracts
 {
@@ -11,6 +12,6 @@ namespace WcfServiceLibrary.Contracts
     public interface IAuthenticationContract
     {
         [OperationContract]
-        bool CheckUserIsRegistered();
+        bool CheckUserIsRegistered(UserDTO user);
     }
 }
