cb725427d391e6cc2ef21930f12fe02cac7c121f
Матвей Кирик <ikirik59@gmail.com>
2019-05-28 23:43:18 +0300

    host configurator was added

---
+++ Hoggy/HoggyServiceHost/HostConfigurator.cs
@@ -0,0 +1,53 @@
+﻿using DataAccessLayer;
+using DependencyInjections;
+using Ninject;
+using System;
+using System.IO;
+using System.ServiceModel;
+using WcfServiceLibrary.Helpers;
+using WcfServiceLibrary.Services;
+
+namespace HoggyServiceHost
+{
+    public class HostConfigurator
+    {
+        IRepository _repository;
+        TextWriter backlog;
+
+        public HostConfigurator(TextWriter backlog)
+        {
+            _repository = Factory.Kernel.Get<IRepository>();
+            MapperConfigurator.Configure();
+        }
+
+        public void Start()
+        {
+            AuthenticationService authService = new AuthenticationService(_repository);
+            ServiceHost authServiceHost = new ServiceHost(authService);
+
+            RegistrationService regService = new RegistrationService(_repository);
+            ServiceHost regServiceHost = new ServiceHost(regService);
+
+            DataExchangeService dataExService = new DataExchangeService(_repository);
+            ServiceHost dataExServiceHost = new ServiceHost(dataExService);
+
+            CreationService creationService = new CreationService(_repository);
+            ServiceHost creationServiceHost = new ServiceHost(creationService);
+
+            CommunityService communityService = new CommunityService(_repository);
+            ServiceHost communityServiceHost = new ServiceHost(communityService);
+
+
+            authServiceHost.Open();
+            backlog.WriteLine("Authantication service is started");
+            regServiceHost.Open();
+            backlog.WriteLine("Registration service is started");
+            dataExServiceHost.Open();
+            backlog.WriteLine("Data exchange service is started");
+            creationServiceHost.Open();
+            backlog.WriteLine("Creation service is started");
+            communityServiceHost.Open();
+            backlog.WriteLine("Community service is started");
+        }
+    }
+}
--- Hoggy/HoggyServiceHost/Program.cs
+++ Hoggy/HoggyServiceHost/Program.cs
@@ -1,17 +1,5 @@
-﻿using DataAccessLayer;
-using DataAccessLayer.Entities;
-using DataTransferObjects;
-using DependencyInjections;
-using Ninject;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
-using WcfServiceLibrary.Helpers;
-using WcfServiceLibrary.Services;
+﻿using System;
+using System.IO;
 
 namespace HoggyServiceHost
 {
@@ -19,38 +7,11 @@ namespace HoggyServiceHost
     {
         static void Main(string[] args)
         {
-            MapperConfigurator.Configure();
+            HostConfigurator configurator = new HostConfigurator(Console.Out);
+            configurator.Start();
+            while (true)
+                Console.ReadLine();
 
-            IRepository repository = Factory.Kernel.Get<IRepository>();
-
-            AuthenticationService authService = new AuthenticationService(repository);
-            ServiceHost authServiceHost = new ServiceHost(authService);
-
-            RegistrationService regService = new RegistrationService(repository);
-            ServiceHost regServiceHost = new ServiceHost(regService);
-
-            DataExchangeService dataExService = new DataExchangeService(repository);
-            ServiceHost dataExServiceHost = new ServiceHost(dataExService);
-
-            CreationService creationService = new CreationService(repository);
-            ServiceHost creationServiceHost = new ServiceHost(creationService);
-
-            CommunityService communityService = new CommunityService(repository);
-            ServiceHost communityServiceHost = new ServiceHost(communityService);
-
-
-            authServiceHost.Open();
-            Console.WriteLine("Authantication service is started");
-            regServiceHost.Open();
-            Console.WriteLine("Registration service is started");
-            dataExServiceHost.Open();
-            Console.WriteLine("Data exchange service is started");
-            creationServiceHost.Open();
-            Console.WriteLine("Creation service is started");
-            communityServiceHost.Open();
-            Console.WriteLine("Community service is started");
-
-            Console.ReadLine();
         }
     }
 }
