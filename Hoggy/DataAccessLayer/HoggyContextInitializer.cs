e5a92753bbde3152eac786bdf0d252664cb7a5bf
Матвей Кирик <ikirik59@gmail.com>
2019-05-29 14:05:28 +0300

    bugfix

--- Hoggy/DataAccessLayer/HoggyContextInitializer.cs
+++ Hoggy/DataAccessLayer/HoggyContextInitializer.cs
@@ -6,7 +6,8 @@ namespace DataAccessLayer
     {
         public override void InitializeDatabase(HoggyContext context)
         {
-            context.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction
+            if(context.Database.Exists())
+                context.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction
     , string.Format("ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE", context.Database.Connection.Database));
             base.InitializeDatabase(context);
         }
