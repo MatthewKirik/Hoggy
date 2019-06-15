9b4d330831f962703d5775a3ecdfedfc9ae1f4f4
Матвей Кирик <ikirik59@gmail.com>
2019-06-15 14:08:02 +0300

    settings

--- Hoggy/TestConsoleClient/Program.cs
+++ Hoggy/TestConsoleClient/Program.cs
@@ -8,8 +8,8 @@ namespace TestConsoleClient
         {
             Generator generator = new Generator();
             //generator.DeletionTest(1, 2, 3, 3, 2);
-            //generator.InitializeHierarchy(1, 2, 3, 3, 2);
-            generator.TeamHierarchy(4, 1, 3, 3, 2);
+            generator.InitializeHierarchy(2, 2, 3, 3, 2);
+            //generator.TeamHierarchy(4, 1, 3, 3, 2);
             //generator.CreateGroup(10);
             //generator.TestFiles();
            // generator.EditionTest();
